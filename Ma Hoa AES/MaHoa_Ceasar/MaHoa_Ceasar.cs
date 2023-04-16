0f801104bd7a8f9b7c78953fb4f83b357af14a18
Khuongpham2001 <phamgiakhuong280601@gmail.com>
2023-04-16 09:05:11 +0700

    giao dien 2

--- Ma Hoa AES/MaHoa_Ceasar/MaHoa_Ceasar.cs
+++ Ma Hoa AES/MaHoa_Ceasar/MaHoa_Ceasar.cs
@@ -21,5 +21,65 @@ namespace Ma_Hoa_AES.MaHoa_Ceasar
         {
 
         }
+
+        private void thuậtToánCeasarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thuậtToánTrimethiusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trangChủToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thuậtToánJCeasarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            GiaoDien_TrangChu.GiaoDien_TrangChu form2 = new GiaoDien_TrangChu.GiaoDien_TrangChu();
+            form2.ShowDialog();
+            this.Close();
+        }
+
+        private void thuậtToánTrimethiusToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            MaHoa_Trimethius.MaHoa_Trimethius form2 = new MaHoa_Trimethius.MaHoa_Trimethius();
+            form2.ShowDialog();
+            this.Close();
+        }
+
+        private void Form2Ceasar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2Belesco_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MaHoa_Belasco.MaHoa_Belasco form2 = new MaHoa_Belasco.MaHoa_Belasco();
+            form2.ShowDialog();
+            this.Close();
+        }
+
+        private void Form2Vignere_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MaHoa_Vignere.MaHoa_Vignere form2 = new MaHoa_Vignere.MaHoa_Vignere();
+            form2.ShowDialog();
+            this.Close();
+        }
+
+        private void Form2Xor_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MaHoa_Xor.MaHoa_Xor form2 = new MaHoa_Xor.MaHoa_Xor();
+            form2.ShowDialog();
+            this.Close();
+        }
     }
 }
